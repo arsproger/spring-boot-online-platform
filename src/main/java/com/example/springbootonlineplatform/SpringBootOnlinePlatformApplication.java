ba5d7a0a30cbb411df64e02bf7fa29f7455c2f14
Arsen <arsproger@gmail.com>
2023-04-30 23:23:05 +0600

    initial commit

---
+++ src/main/java/com/example/springbootonlineplatform/SpringBootOnlinePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootonlineplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOnlinePlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootOnlinePlatformApplication.class, args);
+    }
+
+}
