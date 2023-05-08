f7a785ba70ad09b2cd7d615ec3c8a163cd12e769
Arsen <arsproger@gmail.com>
2023-05-08 23:00:16 +0600

    add change UserService, add interface

---
+++ backend/src/main/java/com/it/academy/services/UserService.java
@@ -0,0 +1,17 @@
+package com.it.academy.services;
+
+import com.it.academy.models.User;
+
+import java.util.List;
+
+public interface UserService {
+    List<User> getAll();
+
+    User getById(Long id);
+
+    Long save(User user);
+
+    Long deleteById(Long id);
+
+    Long updateById(Long id, User updatedUser);
+}
