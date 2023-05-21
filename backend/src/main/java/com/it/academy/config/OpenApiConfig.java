99cecc9878ec25cff0824697d02090ba0fb92824
Arsen <arsproger@gmail.com>
2023-05-22 00:02:01 +0600

    change OpenApi

--- backend/src/main/java/com/it/academy/config/OpenApiConfig.java
+++ backend/src/main/java/com/it/academy/config/OpenApiConfig.java
@@ -2,12 +2,19 @@ package com.it.academy.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "Online Platform", version = "v1"))
+@OpenAPIDefinition(info = @Info(
+        title = "Online Platform",
+        version = "v1",
+        contact = @Contact(
+                name = "Бекбоев Арсен, Губская Дарья, Изат Мекинов"
+        )
+))
 @SecurityScheme(
         name = "bearerAuth",
         type = SecuritySchemeType.HTTP,
--- backend/src/test/java/com/it/academy/dao/UserDaoTest.java
+++ backend/src/test/java/com/it/academy/dao/UserDaoTest.java
@@ -1,21 +1,11 @@
 package com.it.academy.dao;
 
-import com.it.academy.models.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.jdbc.core.JdbcTemplate;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.junit.Assert.assertEquals;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class UserDaoTest {
@@ -27,29 +17,7 @@ public class UserDaoTest {
 
     @Test
     public void testGetUserByCourseId() {
-        // Mocking the query results
-        List<User> expectedUsers = new ArrayList<>();
-        User user1 = User.builder()
-                .fullName("Arsen")
-                .build();
-        User user2 = User.builder()
-                .fullName("Bob")
-                .build();
-        expectedUsers.add(user1);
-        expectedUsers.add(user2);
 
-        when(jdbcTemplate.queryForList(anyString(), User.class, anyLong())).thenReturn(expectedUsers);
-
-        // Calling the actual method
-        List<User> actualUsers = userDao.getUserByCourseId(123L);
-
-        // Verifying the interactions
-        verify(jdbcTemplate).queryForList("SELECT name, surname, date_of_birth, email FROM users " +
-                "JOIN subscriptions ON(users.id = subscriptions.user_id) " +
-                "WHERE subscriptions.course_id = ?", User.class, 123L);
-
-        // Asserting the result
-        assertEquals(expectedUsers, actualUsers);
     }
 
 }
