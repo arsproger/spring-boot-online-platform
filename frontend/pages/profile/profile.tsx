5d85097e7beec35cf0bc828b573c4be42cec2db4
Izatis <mekinovizat1@gmail.com>
2023-05-23 03:28:50 +0600

    add: page profile

---
+++ frontend/pages/profile/profile.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from "./profile.module.scss";
+
+const Profile = () => {
+    return (
+        <div className={s.profile}> 
+            Profile
+        </div>
+    );
+};
+
+export default Profile;
