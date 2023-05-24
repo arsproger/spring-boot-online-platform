554e570d99e376eced5ab3dda6208e79ad67742d
daria <gybskaya.dd@gmail.com>
2023-05-24 15:23:45 +0600

    add row mapper for lesson

--- backend/src/main/java/com/it/academy/dao/LessonDao.java
+++ backend/src/main/java/com/it/academy/dao/LessonDao.java
@@ -1,5 +1,6 @@
 package com.it.academy.dao;
 
+import com.it.academy.dao.rowMapper.LessonRowMapper;
 import com.it.academy.models.Lesson;
 import lombok.AllArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -11,10 +12,11 @@ import java.util.List;
 @AllArgsConstructor
 public class LessonDao {
     private final JdbcTemplate jdbcTemplate;
+    private final LessonRowMapper lessonRowMapper;
 
     public List<Lesson> filterByDuration(Integer from, Integer to) {
-        return jdbcTemplate.queryForList("SELECT * FROM lessons WHERE duration between ? and ?",
-                Lesson.class, from, to);
+        return jdbcTemplate.query("SELECT * FROM lessons WHERE duration between ? and ?",
+                new LessonRowMapper(), from, to);
     }
 
 }
