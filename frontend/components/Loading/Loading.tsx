841cdda763d8d25091e04a14184cf85e526f46fb
Arsen <arsproger@gmail.com>
2023-05-24 22:03:52 +0600

    delete frontend directory

--- frontend/components/Loading/Loading.tsx
+++
@@ -1,9 +0,0 @@
-import React, { FC } from "react";
-import s from "./Loading.module.scss";
-
-const Loading: FC = () => {
-  return <div className={s.loading}></div>;
-};
-
-export default Loading;
-
