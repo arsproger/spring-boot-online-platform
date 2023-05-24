a32239a560f63e3d9fedc950ded096ac21837067
daria <gybskaya.dd@gmail.com>
2023-05-24 15:24:39 +0600

    add row mapper for subscription

--- backend/src/main/java/com/it/academy/dao/SubscriptionDao.java
+++ backend/src/main/java/com/it/academy/dao/SubscriptionDao.java
@@ -1,42 +1,22 @@
 package com.it.academy.dao;
 
-import com.it.academy.models.Course;
+import com.it.academy.dao.rowMapper.SubscriptionRowMapper;
 import com.it.academy.models.Subscription;
-import com.it.academy.models.User;
 import lombok.AllArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
 @Component
 @AllArgsConstructor
 public class SubscriptionDao {
     private final JdbcTemplate jdbcTemplate;
+    private final SubscriptionRowMapper subscriptionRowMapper;
 
     public List<Subscription> getActiveSubscriptionsByUserId(Long userId) {
         String sql = "SELECT * FROM subscriptions WHERE user_id =?;";
         return jdbcTemplate.query(sql, new SubscriptionRowMapper(), userId);
     }
 
-    private static class SubscriptionRowMapper implements RowMapper<Subscription> {
-
-        @Override
-        public Subscription mapRow(ResultSet rs, int rowNum) throws SQLException {
-            Subscription subscription = new Subscription();
-            Course course = new Course();
-            course.setId(rs.getLong("course_id"));
-            User user = new User();
-            user.setId(rs.getLong("user_id"));
-
-            subscription.setId(rs.getLong("id"));
-            subscription.setCourse(course);
-            subscription.setCreationDate(rs.getDate("creation_date").toLocalDate());
-            subscription.setUser(user);
-            return subscription;
-        }
-    }
 }
