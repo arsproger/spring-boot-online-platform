f80a297959ff6619998704132ba7c753c1b3a78e
daria <gybskaya.dd@gmail.com>
2023-05-25 19:38:36 +0600

    refactoring

--- backend/src/main/java/com/it/academy/config/DaoConfig.java
+++ backend/src/main/java/com/it/academy/config/DaoConfig.java
@@ -12,7 +12,7 @@ import javax.sql.DataSource;
 import java.util.Objects;
 
 @Configuration
-@PropertySource("classpath:application.properties")
+@PropertySource("classpath:application.yml")
 @AllArgsConstructor
 public class DaoConfig {
     private final Environment environment;
