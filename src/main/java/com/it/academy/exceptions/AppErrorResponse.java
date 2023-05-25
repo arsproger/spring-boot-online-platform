54fe84272c74c99afc70a619448f4bcfc802e654
Arsen <arsproger@gmail.com>
2023-05-25 18:40:39 +0600

    fix starting bags

--- src/main/java/com/it/academy/exceptions/AppErrorResponse.java
+++ src/main/java/com/it/academy/exceptions/AppErrorResponse.java
@@ -3,9 +3,7 @@ package com.it.academy.exceptions;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import org.springframework.stereotype.Component;
 
-@Component
 @AllArgsConstructor
 @Builder
 @Data
--- src/main/java/com/it/academy/exceptions/AppException.java
+++ src/main/java/com/it/academy/exceptions/AppException.java
@@ -1,16 +1,20 @@
 package com.it.academy.exceptions;
 
-import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 
-@Getter
 public class AppException extends RuntimeException {
     private final HttpStatus code;
 
     public AppException(String message, HttpStatus code) {
+
         super(message);
         this.code = code;
+
+    }
+
+    public HttpStatus getCode() {
+        return code;
     }
 
 }
